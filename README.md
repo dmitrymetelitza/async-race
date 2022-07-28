42cc194e09a9207ec06e86ec8eabe45b898e7733
Dzmitry Miatelitsa <Dzmitry>
2022-07-28 10:58:22 +0300

    init: start task async race

---
+++ README.md
@@ -0,0 +1,3 @@
+# agm234-JSFE2021Q1
+Task "Async Race" https://github.com/rolling-scopes-school/tasks/blob/master/tasks/async-race.md#cross-check
+
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "printWidth": 120,
+    "tabWidth": 2,
+    "semi": true,
+    "bracketSpacing": true,
+    "singleQuote": true,
+    "endOfLine": "auto"
+}
