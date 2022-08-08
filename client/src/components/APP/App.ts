2f12695af8e45c45463ba3b59488449623f2ef57
Dzmitry Miatelitsa <Dzmitry>
2022-08-08 18:41:03 +0300

    feat: add app

--- client/src/components/APP/App.ts
+++ client/src/components/APP/App.ts
@@ -1,11 +1,18 @@
 import { Component } from "../utils/component";
 import { Header } from "../header/header";
+import { Router } from "../router";
 
 export class App{
     private main;
+    private router;
 
     constructor(private rootElement: HTMLAnchorElement){
         const header = new Header(this.rootElement);
         this.main = new Component(this.rootElement, 'main', ['main']);
+
+        this.router = new Router(this.main.element)
+    }
+    init(): void { 
+        this.router.initRouter()
     }
 }
