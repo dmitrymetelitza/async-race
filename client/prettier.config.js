4c26a7db0ff81301bad6b826b78aa7cede53436b
Dzmitry Miatelitsa <Dzmitry>
2022-08-08 23:32:08 +0300

    sas

--- client/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 120,
-  tabWidth: 2,
-  semi: true,
-  bracketSpacing: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  endOfLine: 'auto',
-};
--- client/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { App } from './app';
-import './index.scss';
-
-window.addEventListener('DOMContentLoaded', () => {
-  const rootElement = document.body;
-  const app = new App(rootElement);
-
-  app.init();
-});
